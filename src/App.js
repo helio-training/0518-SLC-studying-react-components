e3208caa473269c8299a5ecd42ed1bde85bc7e5e
Tyler Garlick <tgarlick@heliotraining.com>
2018-06-04 15:35:32 -0600

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css'
+import NameBadge from './NameBadge'
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <NameBadge />
+      </div>
+    )
+  }
+}
---
+++ src/NameBadge.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          Hello,
+        </div>
+        <div>
+          my name is...
+        </div>
+        <input type="text" name="name" placeholder="Your Name" />
+      </div>
+    )
+  }
+}
+
